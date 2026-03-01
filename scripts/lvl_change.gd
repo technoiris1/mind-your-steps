0a98c2092c5ca3260a5730080e13861ee50640db
Manan Sharma <83415076+technoiris1@users.noreply.github.com>
2026-03-01 15:43:53 +0530

    some things

--- scripts/lvl_change.gd
+++ scripts/lvl_change.gd
@@ -10,14 +10,23 @@ func _on_body_entered(_body):
 	if triggered:
 		return
 	triggered = true
-	print("hi")
-	await get_tree().create_timer(3.0)
-	get_tree().change_scene_to_file("res://scenes/msg.tscn")
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 3.0
+	timer.one_shot = true
+	timer.timeout.connect(_change_scene)
+	timer.start()
 
 func _on_area_entered(_area):
 	if triggered:
 		return
 	triggered = true
-	print("hi")
-	await get_tree().create_timer(3.0)
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 3.0
+	timer.one_shot = true
+	timer.timeout.connect(_change_scene)
+	timer.start()
+
+func _change_scene():
 	get_tree().change_scene_to_file("res://scenes/msg.tscn")
